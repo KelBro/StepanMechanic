50b1ddc5074b49dd184037d4e504c6f98d265bb0
kofeyok58 <zlatakupnevich@gmail.com>
2024-01-04 17:54:50 +1000

    name

--- main.py
+++ main.py
@@ -16,25 +16,24 @@ def load_image(name, colorkey=None):
 
 def start_screen():
     pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
+    pygame.display.set_caption('menu')
     size = WIDTH, HEIGHT = 800, 400
     screen = pygame.display.set_mode(size)
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ["СТЕПАН МЕХАНИК-ПЕРЕКУПЩИК",
+                  "ВЕРСИЯ 347593459874982"
+                  ]
 
     clock = pygame.time.Clock()
     fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 50
+    text_coord = 30
     for line in intro_text:
         string_rendered = font.render(line, 1, pygame.Color('black'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = 300
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
     running = True
